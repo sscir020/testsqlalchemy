79351f3e3a98e228103e133aa8f65b5ec590f30c
zhangjy <1523883977@qq.com>
2018-06-26 17:57:05 +0800

    css sql

--- main_run.py
+++ main_run.py
@@ -2,4 +2,5 @@ from app import create_app
 if __name__=='__main__':
     # app=create_app('testing')
     app=create_app('development')
-    app.run(port=6006,debug=True)
+    app.run(port=6006
+            ,debug=True)
